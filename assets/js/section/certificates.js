41c89fa0a0e8c62e0dc3d479c6166168de592d11
Arga Hutama <argahut@gmail.com>
2023-09-17 10:46:23 +0700

    Update certificates and projects

--- assets/js/section/certificates.js
+++ assets/js/section/certificates.js
@@ -4,62 +4,74 @@ function generateCertificateItems() {
       certificateIssuer: "Dicoding",
       certificateName: "Menjadi iOS Developer Expert",
       issued: "Dec 2022",
+      url: "https://www.dicoding.com/certificates/NVP7943VWZR0",
     },
     {
       certificateIssuer: "Udemy",
       certificateName: "Pemrograman Go-Lang",
       issued: "Apr 2022",
+      url: "http://ude.my/UC-57f31a9b-2916-4136-b085-b433e88971e3",
     },
     {
       certificateIssuer: "Dicoding",
       certificateName: "Menjadi Android Developer Expert",
       issued: "Dec 2021",
+      url: "https://dicoding.com/certificates/RVZK176KOPD5",
     },
     {
       certificateIssuer: "Dicoding",
       certificateName: "Belajar Fundamental Aplikasi Flutter",
       issued: "Nov 2021",
+      url: "https://www.dicoding.com/certificates/0LZ03NDKRZ65",
     },
     {
       certificateIssuer: "Dicoding",
       certificateName: "Belajar Android Jetpack Pro",
       issued: "Jun 2021",
+      url: "https://www.dicoding.com/certificates/72ZD2NLOJZYW",
     },
     {
       certificateIssuer: "Udemy",
       certificateName: "Android Jetpack Masterclass in Kotlin",
       issued: "Apr 2021",
+      url: "http://ude.my/UC-0323a87c-b826-48dc-8ab4-b5526607b66e/",
     },
     {
       certificateIssuer: "Udemy",
       certificateName:
         "The Complete Android 12 & Kotlin Development Masterclass",
       issued: "Nov 2020",
+      url: "http://ude.my/UC-7f6d989d-1188-4636-86be-dbba12234b98",
     },
     {
       certificateIssuer: "Udemy",
       certificateName: "The Complete Flutter Development Bootcamp with Dart",
       issued: "Sep 2020",
+      url: "http://ude.my/UC-8b4a325e-789b-4fcf-aeb0-2198497a48ab",
     },
     {
       certificateIssuer: "Dicoding Indonesia",
       certificateName: "Belajar Prinsip Pemrograman SOLID",
       issued: "Aug 2020",
+      url: "https://www.dicoding.com/users/589462/academies",
     },
     {
       certificateIssuer: "Cambridge Certification Authority",
       certificateName: "CCA - Java Level 2",
       issued: "Aug 2020",
+      url: "https://drive.google.com/file/d/1NMTROJ3CdkY5evaOTRRwsvJ0dyQwbV_7/view?usp=drive_link",
     },
     {
       certificateIssuer: "Cambridge Certification Authority",
       certificateName: "CCA - Java Level 1",
       issued: "Jul 2020",
+      url: "https://drive.google.com/file/d/1XFIT1PeV4UTl3CO9bG6iOjfCUG8im2Wr/view?usp=drive_link",
     },
     {
       certificateIssuer: "Dicoding Indonesia",
       certificateName: "Memulai Pemrograman dengan Kotlin",
       issued: "May 2020",
+      url: "https://www.dicoding.com/users/589462/academies",
     },
   ];
 
@@ -67,10 +79,14 @@ function generateCertificateItems() {
 
   certificateItems.forEach((item) => {
     const section = document.createElement("section");
+    const urlHTML = item.url ? `<a href="${item.url}" target="_blank">` : "";
+    const closingTag = item.url ? "</a>" : "";
+
     section.innerHTML = `
-          <h4>${item.certificateIssuer} - ${item.certificateName}</h4>
-          <p>Issued: ${item.issued}</p>
-        `;
+      <h4>${urlHTML}${item.certificateIssuer} - ${item.certificateName}${closingTag}</h4>
+      <p>Issued: ${item.issued}</p>
+    `;
+
     certificateContainer.appendChild(section);
   });
 }
--- assets/js/section/projects.js
+++ assets/js/section/projects.js
@@ -3,30 +3,40 @@ function generateProjectItems() {
     {
       title: "GetPlus",
       imageSrc: "assets/images/project/getplus.png",
+      url: "https://play.google.com/store/apps/details?id=com.getplus.application",
+      downloads: "500K+",
       description:
         "GetPlus is a multi-brand loyalty program that offers you the fastest way to earn points across all payment methods out of your everyday shopping. From groceries to travel, banking to fashion, GetPlus offers deals by well-known brands. ",
     },
     {
       title: "Zenius",
       imageSrc: "assets/images/project/zenius.png",
+      url: "https://play.google.com/store/apps/details?id=net.zenius.mobile",
+      downloads: "5M+",
       description:
         "An online learning application that can help you keep track of school materials and focus on exam preparation, from PTS, PAS, PAT, UTBK, to Independent Examinations (SIMAK UI, UM UGM, etc.)!",
     },
     {
       title: "Zenpro",
       imageSrc: "assets/images/project/zenpro.png",
+      url: "https://play.google.com/store/apps/details?id=net.zenpro.mobile",
+      downloads: "50K+",
       description:
         "Certified Training Platform for Professionals, Pre-Employment Card Participants and New Graduates!",
     },
     {
       title: "OKHOME",
       imageSrc: "assets/images/project/okhome.png",
+      url: "https://play.google.com/store/apps/details?id=id.co.okhome.okhomeapp",
+      downloads: "100K+",
       description:
         "OKHOME is reliable and trusted lifestyle service in Indonesia. OKHOME provides residential cleaning services, AC cleaning services, and disinfectants for houses and apartments.",
     },
     {
       title: "Planet Indonesia",
       imageSrc: "assets/images/project/planet.webp",
+      url: "https://play.google.com/store/apps/details?id=com.oddy.customer",
+      downloads: "100K+",
       description:
         "PLANET helps people maintain their vehicle with proper service, easy to order, scheduled and recorded in detail.",
     },
@@ -36,11 +46,21 @@ function generateProjectItems() {
 
   projectItems.forEach((item) => {
     const section = document.createElement("section");
+    const hasUrl = !!item.url;
+
     section.innerHTML = `
-          <h4>${item.title}</h4>
-          <img class="featured-image rounded-image" src="${item.imageSrc}" alt="${item.title}" />
-          <p>${item.description}</p>
-        `;
+      <h4>${
+        hasUrl
+          ? `<a href="${item.url}" target="_blank">${item.title}</a>`
+          : item.title
+      }</h4>
+      <img class="featured-image rounded-image" src="${item.imageSrc}" alt="${
+      item.title
+    }" />
+      <p>${item.description}</p>
+      <p>Downloads: <b>${item.downloads}<b/></p>
+    `;
+
     projectContainer.appendChild(section);
   });
 }
